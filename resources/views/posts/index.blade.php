0ae183ff31805062b776e473471c1299daed275e
yuran09 <yuranfgs@gmail.com>
2021-10-04 13:12:02 +0200

    fazer likes e dislikes bem como ver likes unsigned

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,7 @@
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
 
-            <form action="{{route('posts')}}" method="post">
+            <form action="{{route('posts')}}" method="post" class="mb-4">
                 @csrf
                 <div class="mb-4">
                     <label for="body" class="sr-only">Body</label>
@@ -23,6 +23,47 @@
                     <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Post</button>
                 </div>
             </form>
+
+            @if($posts->count())
+                @foreach ($posts as $post)
+                    <div class="mb-4">
+                        <a href="" class="font-bold">
+                            {{$post->user->name}}
+                        </a>
+                        <span class="text-grey-600 text-sm">
+                            {{$post->user->created_at->diffForHumans()}}
+                        </span>
+                        <p class="mb-1">
+                            {{$post->body}}
+                        </p>
+
+                        <div class="flex items-center">
+                            @auth
+                                @if(!$post->likedby(auth()->user()))
+                                    <form action="{{route('posts.likes', $post)}}" method="post" class="mr-1">
+                                        @csrf
+                                        <button type="submit" class="text-blue-500">Like</button>
+                                    </form>
+                                @else
+                                    <form action="{{route('posts.likes', $post)}}" method="post" class="mr-1">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="text-blue-500">Unlike</button>
+                                    </form>
+                                @endif
+                            @endauth
+                            <span>
+                                {{$post->likes->count()}} {{Str::plural('like', $post->likes->count())}}
+                            </span>
+                        </div>
+                    </div>
+
+                @endforeach
+                {{$posts->links()}}
+            @else
+                <p>There are no posts</p>
+            @endif
+
         </div>
     </div>
 @endsection
